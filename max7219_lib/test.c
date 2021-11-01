fcb6f48d9d0baa4f1ac8962d5ce82f198d64d247
flalini <flan101544@gmail.com>
2021-11-02 02:05:30 +0900

    max7219_lib 002

--- max7219_lib/test.c
+++ max7219_lib/test.c
@@ -6,7 +6,7 @@
 /*   By: ijang <flan101544@gmail.com>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/02 01:14:01 by ijang             #+#    #+#             */
-/*   Updated: 2021/11/02 01:32:43 by ijang            ###   ########.fr       */
+/*   Updated: 2021/11/02 02:04:20 by ijang            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,14 +28,14 @@ int		main(void)
 			perror("");
 		exit(errno);
 	}
-	ms->state[0] = 0b00000000;
-	ms->state[1] = 0b01000000;
-	ms->state[2] = 0b01100000;
-	ms->state[3] = 0b01110000;
-	ms->state[4] = 0b01111100;
-	ms->state[5] = 0b01111110;
-	ms->state[6] = 0b01111111;
-	ms->state[7] = 0b01111111;
+	ms->state[0][0] = 0b00000000;
+	ms->state[1][0] = 0b01000000;
+	ms->state[2][0] = 0b01100000;
+	ms->state[3][0] = 0b01110000;
+	ms->state[4][0] = 0b01111100;
+	ms->state[5][0] = 0b01111110;
+	ms->state[6][0] = 0b01111111;
+	ms->state[7][0] = 0b01111111;
 	spi_matrix_print(ms);
 	spi_2byte_print(SHUTDOWN, 0x00);
 	exit(0);
