b8ebc76ccd626ef25fd881e197c3ceb89487a9ce
flalini <flan101544@gmail.com>
2021-11-03 19:45:37 +0900

    max7219 test modify

--- max7219_lib/test.c
+++ max7219_lib/test.c
@@ -6,7 +6,7 @@
 /*   By: ijang <flan101544@gmail.com>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/02 01:14:01 by ijang             #+#    #+#             */
-/*   Updated: 2021/11/02 14:38:41 by ijang            ###   ########.fr       */
+/*   Updated: 2021/11/03 19:45:00 by ijang            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,6 +28,7 @@ int		main(void)
 			perror("");
 		exit(errno);
 	}
+	/*
 	ms->state[0][0] = 0b00000000;
 	ms->state[1][0] = 0b01000000;
 	ms->state[2][0] = 0b01100000;
@@ -35,8 +36,23 @@ int		main(void)
 	ms->state[4][0] = 0b01111000;
 	ms->state[5][0] = 0b01111100;
 	ms->state[6][0] = 0b01111110;
-	ms->state[7][0] = 0b01111111;
+	ms->state[7][0] = 0b01111111;*/
+	ms->state[0][0] = 0b00000001;
+	ms->state[1][0] = 0b00000001;
+	ms->state[2][0] = 0b00000001;
+	ms->state[3][0] = 0b00000001;
+	ms->state[4][0] = 0b00000001;
+	ms->state[5][0] = 0b00000001;
+	ms->state[6][0] = 0b00000001;
+	ms->state[7][0] = 0b00000001;
 	spi_matrix_print(ms);
+	delay(2000);
+	for (int i = 0; i < 8; ++i)
+		for (int j = 0; j < i; ++j) {
+			ms->state[i][0] <<= 1;
+			spi_matrix_print(ms);
+			delay(200);
+		}
 	delay(2000);
 	spi_2byte_print(SHUTDOWN, 0x00);
 	ms_free(ms);
